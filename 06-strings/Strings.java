0db05b83910300af14ace3d49c6ca5bd80cc9727
Mohammed Ali <mohammeda258@nycstudents.net>
2020-10-26 20:51:22 -0400

    Added a feature with string

---
+++ 06-strings/Strings.java
@@ -0,0 +1,9 @@
+public class Strings{
+   public static void main(String args[]) {
+
+      String word = "Hello world";
+
+      char Letter = word.charAt(4);
+      System.out.println("The letter at index 4 is: "+ Letter);
+   }
+}
