0ad14b7d756bc7a0308e72358fb3b0a4c6f5dc75
Mohammed Ali <mohammeda258@nycstudents.net>
2020-12-01 18:58:47 -0500

    Made a plan for 2020 day one but I want not sure how to do day 1 2019

---
+++ 09-advent/Day12019.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Day12019 {
+  public static void main(String[] args) {
+
+    try{
+      //Not sure how i would get that file.....
+      Scanner s = new Scanner(new File("data.txt"));
+      while(s.hasNext()){
+        String W = (s.next());
+      }//end while loop
+      s.close();
+    }//end try
+
+    catch(Exception e){
+      System.out.println("file not found");
+    }//end catch
+  }
+  for(int i = 0; i < s; i++){
+    
+  }
+}
---
+++ 09-advent/Day12020Plan.java
@@ -0,0 +1,16 @@
+public class Day12020Plan {
+  public static void main(String[] args) {
+    //I plan to work with arrylistes
+    //I plan to make a list of the numbers then I woud to put them in a loop and take in each number and add that number with every number in the list
+    //For example lets say there are 4,6,7,8,3,4,5,8,9,0,1.(I plan to make it so 4+6 then 4+7, 4+8 etc and do that with every number)
+    //Make list(The numbers)
+  }
+  //For loop(i=0,i<list,i++)
+  //list will plus one everytime the loop happens
+  //inloop* (add list(a) + list(i))
+  //inloop*if(list(a) == last place in the list +1)
+  //What i mean is that if the first number is done add with all the numbers i will add +1 to that number so it moves onto number 2 and the loop will happen until we find Day12020Plan
+  //if(list(a)+list(i) == 2020)
+  //list(a)*list(i)
+  //Print ans...
+}
