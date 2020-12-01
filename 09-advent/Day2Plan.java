48915cfc9c63a27d10399b72a2349560c6b1db93
Mohammed Ali <mohammeda258@nycstudents.net>
2020-12-01 18:00:11 -0500

    Plan and day one

---
+++ 09-advent/Day2Plan.java
@@ -0,0 +1,10 @@
+public class Day2Plan {
+  public static void main(String[] args) {
+    //Create variable for L,W,H
+    //Create an area variable and area will equal to L*H*W
+  }
+  //Here have a method
+  //We will be finding the area(Area1 = L*W*H)
+  //Return that area
+  //Do the same with the others
+}
