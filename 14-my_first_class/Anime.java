73a15e1f43649b402e3288a9ad6ee6d68295099f
Mohammed Ali <mohammeda258@nycstudents.net>
2021-01-18 18:46:06 -0500

    Added more

--- 14-my_first_class/Anime.java
+++ 14-my_first_class/Anime.java
@@ -3,9 +3,12 @@ import java.util.Arrays;
 public class Anime {
   // instance variables
   private String favAnime;
+  private String[] randomAnime;//Holds different animes
+
 
   public Anime(String FA) {
     this.favAnime = FA;
+    this.randomAnime = new String[]{"One piece","Naruto","Bleach","Log Horizon","Dororo","Demon Slayer","Fire force","Overlord","Black Clover","Attack on Titan","Jujutsu kaisen"};
   }//end constructor
 
 
@@ -21,5 +24,12 @@ public class Anime {
   public String toString() {
     return "Favorite anime: " + this.favAnime;
   }//end toString method
+
+  public pickRandom(){
+    Random Anime = new Random();
+    int index = Anime.nextInt(this.randomAnime.length);
+    System.out.println(this.randomAnime[index]);
+    return index;
+  }
 
 }// end class
--- 14-my_first_class/AnimeDriver.java
+++ 14-my_first_class/AnimeDriver.java
@@ -1,10 +1,20 @@
 public class AnimeDriver {
   public static void main(String[] args) {
-    Anime weeb1 = new Anime("One piece");
+    Anime weeb1 = new Anime("Hello their my favorite anime is One piece");
 
     weeb1.setAnime("One piece"); //setter method (mutator)
     System.out.println(weeb1);
 
     System.out.println(weeb1.getAnime());
+    System.out.print("Do you want a anime recommendation? (Y/N)")
+    String yn = input.nextLine();
+    if(yn.equals("Y")){
+      Anime = pickRandom(randomAnime);
+    }
+    else{
+      Sytem.out.print("Alright");
+    }
+
+
   }
 }
